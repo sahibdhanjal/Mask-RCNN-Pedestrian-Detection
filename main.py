b3729fb20ebf99287f085cd4c160a065eb67cedf
sdhanjal <sdhanjal@umich.edu>
2018-04-04 19:03:33 -0400

    Real Time

---
+++ main.py
@@ -0,0 +1,108 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import utils
+import coco
+import utils
+import model as modellib
+import visualize
+
+import cv2
+import time
+import urllib.request as urllib2
+
+
+if __name__ == '__main__':
+    # Root directory of the project
+    ROOT_DIR = os.getcwd()
+
+    # Directory to save logs and trained model
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+    # Local path to trained weights file
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    # Download COCO trained weights from Releases if needed
+    if not os.path.exists(COCO_MODEL_PATH):
+        utils.download_trained_weights(COCO_MODEL_PATH)
+
+    # Directory of images to run detection on
+    IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+    class InferenceConfig(coco.CocoConfig):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+
+    config = InferenceConfig()
+    config.display()
+
+    # Create model object in inference mode.
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+    # Load weights trained on MS-COCO
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+    # COCO Class names
+    # Index of the class in the list is its ID. For example, to get ID of
+    # the teddy bear class, use: class_names.index('teddy bear')
+
+    class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+                   'bus', 'train', 'truck', 'boat', 'traffic light',
+                   'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+                   'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+                   'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+                   'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+                   'kite', 'baseball bat', 'baseball glove', 'skateboard',
+                   'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+                   'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                   'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                   'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+                   'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+                   'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+                   'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+                   'teddy bear', 'hair drier', 'toothbrush']
+
+
+    plt.ion()
+    size = (16, 16)
+    fig, ax = plt.subplots(1, figsize = size)
+
+    url = "35.3.71.126:8080"
+    link = 'http://' + url + '/video'
+    print('Streaming from: ' + link)
+
+    stream = urllib2.urlopen(link)
+    bytes = bytes()
+
+    # Read until video is completed
+    while True:
+        bytes += stream.read(1024)
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+            start = time.time()
+            results = model.detect([frame], verbose=1)
+            r = results[0]
+            print(time.time() - start)
+
+
+            ##################################################
+            # Image Plotting
+            ##################################################
+            visualize.display_instances(frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'], "Real Time Detection", size, ax, fig)
+
+            # Press esc on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == 27:
+                exit(0)
